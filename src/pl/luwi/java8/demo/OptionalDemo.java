b1baec8d889b6414d35bfdd922f9ba895c465255
LukaszWiktor <lukasz.wiktor@gmail.com>
2014-08-09 11:53:29 +0200

    Optional demo - map and ifPresent

---
+++ src/pl/luwi/java8/demo/OptionalDemo.java
@@ -0,0 +1,26 @@
+package pl.luwi.java8.demo;
+
+import static java.lang.System.out;
+
+import java.util.Optional;
+
+import pl.luwi.java8.demo.model.User;
+
+public class OptionalDemo {
+	
+	public static Optional<User> getUserById(int id) {
+		if (id == 1) {
+			return Optional.of(new User("admin")); 
+		} else {
+			return Optional.empty();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Optional<User> optUser1 = getUserById(1);
+		Optional<User> optUser2 = getUserById(2);
+		
+		optUser1.map(u -> u.getLogin()).ifPresent(out::println);
+		optUser2.map(u -> u.getLogin()).ifPresent(out::println);
+	}
+}
