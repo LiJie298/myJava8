4cb1ff60cb18362d42d7965ae1acd98701c8358b
LukaszWiktor <lukasz.wiktor@gmail.com>
2014-08-09 11:52:51 +0200

    User - sample model class

---
+++ src/pl/luwi/java8/demo/model/User.java
@@ -0,0 +1,15 @@
+package pl.luwi.java8.demo.model;
+
+
+public class User {
+	
+	private String login;
+	
+	public User(String login) {
+		this.login = login;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+}
