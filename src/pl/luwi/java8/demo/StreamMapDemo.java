069b5e7638cf1c8c09c511a5c72d1faf6d5e20de
LukaszWiktor <lukasz.wiktor@gmail.com>
2014-11-10 08:49:15 +0100

    Stream.map demo - for loop style vs stream style

--- src/pl/luwi/java8/demo/StreamMapDemo.java
+++ src/pl/luwi/java8/demo/StreamMapDemo.java
@@ -1,6 +1,9 @@
 package pl.luwi.java8.demo;
 
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import pl.luwi.java8.demo.model.User;
 import pl.luwi.java8.demo.model.User.Role;
@@ -8,13 +11,26 @@ import pl.luwi.java8.demo.model.User.Role;
 public class StreamMapDemo {
  
 	public static void main(String[] args) {
-		Stream<User> users = Stream.of(
+		List<User> users = Arrays.asList(
 				new User("Andy", Role.Admin),
 				new User("Bob", Role.Editor),
 				new User("Chris", Role.Viewer));
 		
-		Stream<User.Role> roles = users.map(u -> u.getRole());
+		// for loop style
 		
-		roles.forEach(r -> System.out.println(r));
+		List<Role> roles1 = new ArrayList<>();
+		for (User u : users) {
+		    roles1.add(u.getRole());
+		}
+		
+		System.out.println(roles1);
+		
+		// stream style
+		
+		List<Role> roles2 = users.stream()
+		        .map(u -> u.getRole())
+		        .collect(Collectors.toList());
+		
+		System.out.println(roles2);
 	}
 }
